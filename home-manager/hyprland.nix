6c994088c374b170e05ec88e0cd4a3a89a45595b
ethernet <arilotter@gmail.com>
2026-07-10 20:21:50 -0400

    lua hyprland

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -158,73 +158,85 @@ in
       ];
 
       # All binds go under the `bind` setting; flags distinguish mouse/locked/repeating.
-      bind =
-        [
-          # basics
-          (h.bind (h.key mod "Return") (h.exec "ghostty"))
-          (h.bind (h.key mod "R") (h.layout "togglesplit"))
-          (h.bind (h.key mod "F") h.window.fullscreenToggle)
-          (h.bind (h.key mod "D") (h.exec ''rofi -show drun -display-drun " " -show-icons''))
-          (h.bind (h.key "${mod} + SHIFT" "Q") h.window.close)
+      bind = [
+        # basics
+        (h.bind (h.key mod "Return") (h.exec "ghostty"))
+        (h.bind (h.key mod "R") (h.layout "togglesplit"))
+        (h.bind (h.key mod "F") h.window.fullscreenToggle)
+        (h.bind (h.key mod "D") (h.exec ''rofi -show drun -display-drun " " -show-icons''))
+        (h.bind (h.key "${mod} + SHIFT" "Q") h.window.close)
 
-          # window / workspace nav
-          (h.bind (h.key mod "Tab") h.workspace.previous)
-          (h.bind (h.key "ALT" "Tab") h.window.cycleNext)
-          (h.bind (h.key "ALT" "Tab") h.window.bringToTop)
-          (h.bind (h.key mod "Space") h.window.floatToggle)
-          (h.bind (h.key "${mod} + SHIFT" "Space") h.window.pseudoToggle)
+        # window / workspace nav
+        (h.bind (h.key mod "Tab") h.workspace.previous)
+        (h.bind (h.key "ALT" "Tab") h.window.cycleNext)
+        (h.bind (h.key "ALT" "Tab") h.window.bringToTop)
+        (h.bind (h.key mod "Space") h.window.floatToggle)
+        (h.bind (h.key "${mod} + SHIFT" "Space") h.window.pseudoToggle)
 
-          # widgets
-          (h.bind (h.key mod "C") (h.exec "ags toggle-window notificationsCenter"))
-          (h.bind (h.key mod "N") (h.exec "ags toggle-window quicksettings"))
+        # widgets
+        (h.bind (h.key mod "C") (h.exec "ags toggle-window notificationsCenter"))
+        (h.bind (h.key mod "N") (h.exec "ags toggle-window quicksettings"))
 
-          # screenshots
-          (h.bind (h.noMod "Print") (h.exec "grimblast copysave output # screenshot"))
-          (h.bind (h.key "SUPER" "S") (h.exec "grimblast copysave active"))
-          (h.bind (h.key "SUPER + SHIFT" "S") (h.exec "grimblast copysave area"))
+        # screenshots
+        (h.bind (h.noMod "Print") (h.exec "grimblast copysave output # screenshot"))
+        (h.bind (h.key "SUPER" "S") (h.exec "grimblast copysave active"))
+        (h.bind (h.key "SUPER + SHIFT" "S") (h.exec "grimblast copysave area"))
 
-          # special workspace
-          (h.bind (h.key "SUPER + SHIFT" "A") (h.window.moveToWorkspace "special"))
-          (h.bind (h.key "SUPER" "A") (h.raw "hl.dsp.workspace.toggle_special()"))
+        # special workspace
+        (h.bind (h.key "SUPER + SHIFT" "A") (h.window.moveToWorkspace "special"))
+        (h.bind (h.key "SUPER" "A") (h.raw "hl.dsp.workspace.toggle_special()"))
 
-          # focus
-          (h.bind (h.key mod "Left") (h.focus.dir "l"))
-          (h.bind (h.key mod "Right") (h.focus.dir "r"))
-          (h.bind (h.key mod "Up") (h.focus.dir "u"))
-          (h.bind (h.key mod "Down") (h.focus.dir "d"))
+        # focus
+        (h.bind (h.key mod "Left") (h.focus.dir "l"))
+        (h.bind (h.key mod "Right") (h.focus.dir "r"))
+        (h.bind (h.key mod "Up") (h.focus.dir "u"))
+        (h.bind (h.key mod "Down") (h.focus.dir "d"))
 
-          # move windows
-          (h.bind (h.key "${mod} + SHIFT" "Left") (h.window.moveDir "l"))
-          (h.bind (h.key "${mod} + SHIFT" "Right") (h.window.moveDir "r"))
-          (h.bind (h.key "${mod} + SHIFT" "Up") (h.window.moveDir "u"))
-          (h.bind (h.key "${mod} + SHIFT" "Down") (h.window.moveDir "d"))
+        # move windows
+        (h.bind (h.key "${mod} + SHIFT" "Left") (h.window.moveDir "l"))
+        (h.bind (h.key "${mod} + SHIFT" "Right") (h.window.moveDir "r"))
+        (h.bind (h.key "${mod} + SHIFT" "Up") (h.window.moveDir "u"))
+        (h.bind (h.key "${mod} + SHIFT" "Down") (h.window.moveDir "d"))
 
-          # scroll workspaces
-          (h.bind (h.key mod "mouse_down") h.workspace.next)
-          (h.bind (h.key mod "mouse_up") h.workspace.prev)
+        # scroll workspaces
+        (h.bind (h.key mod "mouse_down") h.workspace.next)
+        (h.bind (h.key mod "mouse_up") h.workspace.prev)
 
-          # mouse binds
-          (h.bindf (h.key mod "mouse:272") h.window.drag h.mouse)
-          (h.bindf (h.key mod "mouse:273") h.window.resize h.mouse)
+        # mouse binds
+        (h.bindf (h.key mod "mouse:272") h.window.drag h.mouse)
+        (h.bindf (h.key mod "mouse:273") h.window.resize h.mouse)
 
-          # media keys (locked so they work at the lock screen)
-          (h.bindf (h.noMod "XF86AudioPlay") (h.exec "${pkgs.playerctl}/bin/playerctl play-pause") h.locked)
-          (h.bindf (h.noMod "XF86AudioPrev") (h.exec "${pkgs.playerctl}/bin/playerctl previous") h.locked)
-          (h.bindf (h.noMod "XF86AudioNext") (h.exec "${pkgs.playerctl}/bin/playerctl next") h.locked)
+        # media keys (locked so they work at the lock screen)
+        (h.bindf (h.noMod "XF86AudioPlay") (h.exec "${pkgs.playerctl}/bin/playerctl play-pause") h.locked)
+        (h.bindf (h.noMod "XF86AudioPrev") (h.exec "${pkgs.playerctl}/bin/playerctl previous") h.locked)
+        (h.bindf (h.noMod "XF86AudioNext") (h.exec "${pkgs.playerctl}/bin/playerctl next") h.locked)
 
-          # brightness / volume (repeating)
-          (h.bindf (h.noMod "XF86MonBrightnessUp") (h.exec "${pkgs.brightnessctl}/bin/brightnessctl s 5+") h.repeating)
-          (h.bindf (h.noMod "XF86MonBrightnessDown") (h.exec "${pkgs.brightnessctl}/bin/brightnessctl s 5-") h.repeating)
-          (h.bindf (h.noMod "XF86AudioRaiseVolume") (h.exec "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +3%") h.repeating)
-          (h.bindf (h.noMod "XF86AudioLowerVolume") (h.exec "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -3%") h.repeating)
+        # brightness / volume (repeating)
+        (h.bindf (h.noMod "XF86MonBrightnessUp") (h.exec "${pkgs.brightnessctl}/bin/brightnessctl s 5+")
+          h.repeating
+        )
+        (h.bindf (h.noMod "XF86MonBrightnessDown") (h.exec "${pkgs.brightnessctl}/bin/brightnessctl s 5-")
+          h.repeating
+        )
+        (h.bindf (h.noMod "XF86AudioRaiseVolume")
+          (h.exec "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +3%")
+          h.repeating
+        )
+        (h.bindf (h.noMod "XF86AudioLowerVolume")
+          (h.exec "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -3%")
+          h.repeating
+        )
 
-          # mute
-          (h.bindf (h.noMod "XF86AudioMute") (h.exec "${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle") h.locked)
+        # mute
+        (h.bindf (h.noMod "XF86AudioMute")
+          (h.exec "${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle")
+          h.locked
+        )
 
-          # lid switch
-          (h.bindf (h.noMod "switch:on:Lid Switch") (h.exec "systemctl suspend") h.locked)
-        ]
-        ++ (h.workspaceBinds { mod = mod; });
+        # lid switch
+        (h.bindf (h.noMod "switch:on:Lid Switch") (h.exec "systemctl suspend") h.locked)
+      ]
+      ++ (h.workspaceBinds { mod = mod; });
 
       window_rule = [
         (h.floatRule { class = "^(pavucontrol)$"; })
