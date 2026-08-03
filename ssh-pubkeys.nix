941265f63b9de3ea1bc128ad83fc4b3fe3f7d218
ethernet <arilotter@gmail.com>
2026-08-03 12:27:40 -0400

    add iris' key, rekey secrets
    macbook. in home-devices so it gets netrc too. verified iris can decrypt.

--- ssh-pubkeys.nix
+++ ssh-pubkeys.nix
@@ -3,6 +3,7 @@ rec {
   hermes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKaPWTrDp1sp3NUXiM/JXKfivQQ6TLxMy7Fyaq59L7y arilotter@gmail.com";
   sol = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL7Yhctas2HcsJeAa82xj7uMcIa3GWJuhwFPKO9qke93 arilotter@gmail.com";
   casey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICnDDh2c4TfMLowUo3r3zAmKKeDHWtwUubi4Ml9sD4TJ arilotter@gmail.com";
+  iris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGACmqQ07rrYy2g8cjCkLeVkiBRfqFD1EwF27+AU0He+ arilotter@gmail.com";
   android = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOUl42TMh18ALGzChA0v1BQ6vzOJYmqt3EdFeuMaS0vD";
 
   nous-eng = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGifAl2SGcrIZEHkV6hm+zJ7DfyE3Q4MJcqCR0hWgqE5 engineering@nousresearch.com";
@@ -10,6 +11,7 @@ rec {
     luna
     hermes
     sol
+    iris
   ];
 
   physically-safe-devices = home-devices ++ [ casey ];
